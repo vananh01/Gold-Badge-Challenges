e6aef45ed4c6c3948d8090095619a1a72f25734f
Anh Vo <vohoangvananh81@gmail.com>
2021-08-25 11:26:33 -0400

    add Menu

---
+++ Cafe/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe
+{
+    public class Menu
+    {
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public double Price { get; set; }
+
+
+        public Menu() { }
+        public Menu (int MealNumber, string MealName, string Description, string Ingredients, double Price)
+        {
+            MealName = mealNumber;
+            MealName = mealName;
+            Description = description;
+            Ingredients = ingredients;
+            Price = price;
+
+
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a9b1cd09-485b-4069-85f4-4db1afa536b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
